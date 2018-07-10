c4069dcb671c5fb46184264af50133952febf89c
bsaiki0 <bsaiki0@saddleback.edu>
2018-07-09 19:13:17 -0700

    r

---
+++ qt-source/custom_vector.h
@@ -0,0 +1,39 @@
+/*
+ *  2D Graphics Modeler
+ *  July 2018
+ *
+ *  David Epstein <DavidE92@live.com>
+ *  Greg Graffius <ggraffius0@saddleback.edu>
+ *  Justis Ketcham Justis <justis.ketcham@gmail.com>
+ *  Brett Saiki <bksaiki@gmail.com>
+ *
+ *  Descr:   Final project for CS1C at Saddleback College (Summer 2018)
+ *  Teacher: John Kath
+ *
+ *  Draws shapes using QPainter. Shapes can be loaded from a file, added
+ *  to a vector, manipulated and later saved. A login is required to access
+ *  the program.
+ *
+ **/
+/*
+    File: custom_vector.h
+
+    Defines UI features
+ */
+
+#ifndef CUSTOM_VECTOR_H
+#define CUSTOM_VECTOR_H
+
+template<typename T>
+class Vector
+{
+public:
+
+
+
+private:
+    T*  mArray;
+    int m
+};
+
+#endif // CUSTOM_VECTOR_H
