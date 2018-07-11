890cb117616a113aca3dbe53c5efa6da435aa012
bsaiki0 <bsaiki0@saddleback.edu>
2018-07-10 21:52:13 -0700

    Vector (working)

--- qt-source/custom_vector.h
+++ qt-source/custom_vector.h
@@ -24,18 +24,222 @@
 #ifndef CUSTOM_VECTOR_H
 #define CUSTOM_VECTOR_H
 
+#include <assert.h>
+
 template<typename T>
 class Vector
 {
 public:
-    Vector() : mArray(new T[vectorDefaultSize]), mSize(0), mReservedSize(0)
+
+    /* Type aliasing */
+    using iterator = T*;
+    using const_iterator = T;
+
+    /* Default constructor */
+    Vector() : mArray(new T[DEFAULT_SIZE]), mSize(0), mReservedSize(DEFAULT_SIZE)
+    {
+        for(int i = 0; i < mReservedSize; ++i)
+            mArray[i] = T();
+    }
+
+    /* Alternate constructor */
+    explicit Vector(int size)  : mArray(new T[size]), mSize(0), mReservedSize(size)
+    {
+        for(int i = 0; i < mReservedSize; ++i)
+            mArray[i] = T();
+    }
+
+    /* Copy constructor */
+    Vector(const Vector& vec) : mSize(vec.mSize), mReservedSize(vec.mReservedSize)
+    {
+        mArray = new T[mReservedSize];
+        for(int i = 0; i < mReservedSize; ++i)
+            mArray[i] = vec.mArray[i];
+    }
+
+    /* Move constructor */
+    Vector(Vector&& vec) : mArray(vec.mArray), mSize(vec.mSize), mReservedSize(vec.mReservedSize)
+    {
+        vec.mArray = nullptr;
+        vec.mSize = 0;
+        vec.mReservedSize = 0;
+    }
+
+    /* Destructor */
+    ~Vector()
+    {
+        if(mArray != nullptr)
+        {
+            delete[] mArray;
+            mArray = nullptr;
+        }
+    }
+
+    /* Copy assignment */
+    Vector& operator=(const Vector& vec)
+    {
+        if(mArray != nullptr)
+            delete[] mArray;
+
+        mArray = new T[mReservedSize];
+        mSize = vec.mSize;
+        mReservedSize = vec.mReservedSize;
+        for(int i = 0; i < mReservedSize; ++i)
+            mArray[i] = vec.mArray[i];
+    }
+
+    /* Move assignment */
+    Vector& operator=(Vector&& vec)
+    {
+        if(mArray != nullptr)
+            delete[] mArray;
+
+        mArray = vec.mArray;
+        mSize = vec.mSize;
+        mReservedSize = vec.mReservedSize;
+        vec.mArray = nullptr;
+        vec.mSize = 0;
+        vec.mReservedSize = 0;
+    }
+
+    /* Access operator */
+    T& operator[](int n)\
+    {
+        assert(n < mSize);
+        return mArray[n];
+    }
+
+    /* Access operator */
+    const T& operator[](int n) const
+    {
+        assert(n < mSize);
+        return mArray[n];
+    }
+
+    /* Returns an iterator to the first element */
+    iterator begin()
+    {
+        return mArray;
+    }
+
+    /* Returns an iterator to the first element */
+    const_iterator begin() const
+    {
+        return mArray;
+    }
+
+    /* Returns the number of elements allocated in the vector */
+    int capacity() const
+    {
+        return mReservedSize;
+    }
+
+    /* Returns an iterator to the element beyond the last element */
+    iterator end()
+    {
+        return &mArray[mSize];
+    }
+
+    /* Returns an iterator to the element beyond the last element */
+    const_iterator end() const
+    {
+        return &mArray[mSize];
+    }
+
+    /* Removes an element pointed to by a given iterator */
+    iterator erase(iterator it)
+    {
+        for(iterator i = it; i != &mArray[mSize - 1]; ++i)
+            *i = *(i + 1);
+
+        --mSize;
+        return it;
+    }
+
+    /* Inserts a new element before a given iterator */
+    iterator insert(iterator it, const T& value)
+    {
+        if(mSize + 1 > mReservedSize)
+        {
+            T* tmp = mArray;
+            mArray = new T[mReservedSize * 2];
+            for(int i = 0; i < mReservedSize; ++i)
+                mArray[i] = tmp[i];
+            delete[] tmp;
+            mReservedSize *= 2;
+        }
+
+        for(iterator i = &mArray[mSize - 1]; i >= it; --i)
+            *(i + 1) = *i;
+        *it = value;
+        ++mSize;
+
+        return it;
+    }
+
+    /* Deletes the last element */
+    void pop_back()
+    {
+        --mSize;
+    }
+
+    /* Adds an element and the end */
+    void push_back(const T& value)
+    {
+        if(mSize + 1 > mReservedSize)
+        {
+            T* tmp = mArray;
+            mArray = new T[mReservedSize * 2];
+            for(int i = 0; i < mReservedSize; ++i)
+                mArray[i] = tmp[i];
+            delete[] tmp;
+            mReservedSize *= 2;
+        }
+
+        mArray[mSize] = value;
+        ++mSize;
+    }
+
+    /* Resizes the vector if the requested size is large than the current reserved size */
+    void reserve(int size)
+    {
+        if(size > mReservedSize)
+        {
+            T* tmp = mArray;
+            mArray = new T[size];
+            mReservedSize = size;
+
+            for(int i = 0; i < mReservedSize; ++i)
+                mArray[i] = tmp[i];
+            delete[] tmp;
+        }
+    }
+
+    /* Resizes the vector */
+    void resize(int size)
+    {
+        T* tmp = mArray;
+        mArray = new T[size];
+        mReservedSize = size;
+        if(mSize > size)  mSize = size;
+
+        for(int i = 0; i < mReservedSize; ++i)
+            mArray[i] = tmp[i];
+        delete[] tmp;
+    }
+
+    /* Returns the number of elements stored in the vector */
+    int size() const
+    {
+        return mSize;
+    }
 
 private:
     T*  mArray;
     int mSize;
     int mReservedSize;
 
-    static const int vectorDefaultSize = 8;
+    static const int DEFAULT_SIZE = 8;
 };
 
 #endif // CUSTOM_VECTOR_H
