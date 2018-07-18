ca61f4b0d98bfca91c5845bf07ae61939da10d3d
ggraffius0 <ggraffius0@saddleback.edu>
2018-07-17 19:21:40 -0700

    added stuff to snippets
    bla

---
+++ snippets/AddUser.cpp
@@ -0,0 +1,14 @@
+#include "AddUser.h"
+#include "ui_AddUser.h"
+
+AddUser::AddUser(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::AddUser)
+{
+    ui->setupUi(this);
+}
+
+AddUser::~AddUser()
+{
+    delete ui;
+}
