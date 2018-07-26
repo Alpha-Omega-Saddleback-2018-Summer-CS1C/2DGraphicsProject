ab128544ba100f5ea71a5ce271cea562d9cc821b
bsaiki0 <bsaiki0@saddleback.edu>
2018-07-25 18:54:16 -0700

    Finishing Touches

--- documentation/search/classes_1.js
+++ documentation/search/classes_1.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['circle',['Circle',['../classshape_1_1_circle.html',1,'shape']]],
+  ['compare_5fshape_5farea',['compare_shape_area',['../structshape_1_1compare__shape__area.html',1,'shape']]],
+  ['compare_5fshape_5fperimeter',['compare_shape_perimeter',['../structshape_1_1compare__shape__perimeter.html',1,'shape']]],
   ['contact',['contact',['../classcontact.html',1,'']]]
 ];
